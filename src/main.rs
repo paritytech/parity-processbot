843a8674004fd6141876d99aeb1ab7ad9a657d4f
Joseph Mark <sjeohp@gmail.com>
2020-06-16 18:30:37 +0200

    Merge test

--- src/main.rs
+++ src/main.rs
@@ -44,6 +44,7 @@ async fn run() -> anyhow::Result<()> {
 
 	// the bamboo queries can take a long time so only wait for it
 	// on launch. subsequently update in the background.
+	//
 	/*
 	{
 		let db_write = db.write();
