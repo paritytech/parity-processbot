7b3ab9b17c5e34c853b76f876b1b594431710d21
Joseph Mark <sjeohp@gmail.com>
2020-08-25 10:18:06 +0200

    Remove dead code

--- src/duration_ticks.rs
+++
@@ -1,9 +0,0 @@
-pub trait DurationTicks {
-	fn ticks(self, period: u64) -> Option<u64>;
-}
-
-impl DurationTicks for Option<std::time::Duration> {
-	fn ticks(self, period: u64) -> Option<u64> {
-		self.map(|s| s.as_secs() / period)
-	}
-}
