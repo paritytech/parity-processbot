2da527fbc9adb244263d2ff7ba3608c4523cbf1c
Haiko Schol <539509+haikoschol@users.noreply.github.com>
2021-01-21 12:14:19 +0100

    Rename Gitlab related env/config variables
    The burn-in automation is not managed on gitlab.parity.io. Renaming the
    config variables is meant to make this explicit.

--- src/main.rs
+++ src/main.rs
@@ -43,11 +43,14 @@ async fn run() -> anyhow::Result<()> {
 	)
 	.await?;
 
-	log::info!("Connecting to Gitlab https://{}", config.gitlab_hostname);
+	log::info!(
+		"Connecting to Gitlab https://{}",
+		config.burnin_gitlab_hostname
+	);
 	let gitlab_bot = gitlab_bot::GitlabBot::new_with_token(
-		&config.gitlab_hostname,
-		&config.gitlab_project,
-		&config.gitlab_private_token,
+		&config.burnin_gitlab_hostname,
+		&config.burnin_gitlab_project,
+		&config.burnin_gitlab_token,
 	)
 	.await?;
 
