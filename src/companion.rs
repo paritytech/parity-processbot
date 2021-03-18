518f0ddb7811ad6f7215a2c7730ff64acf820520
joao-paulo-parity <joao-paulo@parity.io>
2021-03-18 11:31:10 -0300

    enhance companion parsing tests' and pattern

--- src/companion.rs
+++ src/companion.rs
@@ -237,7 +237,7 @@ pub fn companion_parse(body: &str) -> Option<(String, String, String, i64)> {
 
 fn companion_parse_long(body: &str) -> Option<(String, String, String, i64)> {
 	let re = Regex::new(
-		r"companion.*(?P<html_url>github.com/(?P<owner>[^/]+)/(?P<repo>[^/]+)/pull/(?P<number>[[:digit:]]+))"
+		r"companion[^[[:alpha:]]\n]*(?P<html_url>https://github.com/(?P<owner>[^/\n]+)/(?P<repo>[^/\n]+)/pull/(?P<number>[[:digit:]]+))"
 	)
 	.unwrap();
 	let caps = re.captures(&body)?;
@@ -255,7 +255,7 @@ fn companion_parse_long(body: &str) -> Option<(String, String, String, i64)> {
 
 fn companion_parse_short(body: &str) -> Option<(String, String, String, i64)> {
 	let re = Regex::new(
-		r"companion.*(?P<owner>[^/]+)/(?P<repo>[^/]+)#(?P<number>[[:digit:]]+)",
+		r"companion[^[[:alpha:]]\n]*(?P<owner>[^/\n]+)/(?P<repo>[^/\n]+)#(?P<number>[[:digit:]]+)",
 	)
 	.unwrap();
 	let caps = re.captures(&body)?;
@@ -278,74 +278,73 @@ fn companion_parse_short(body: &str) -> Option<(String, String, String, i64)> {
 
 #[cfg(test)]
 mod tests {
-	//use super::*;
+	use super::*;
 
-	//#[test]
-	//fn test_companion_parse() {
-	//assert_eq!(
-	//companion_parse(
-	//"companion: https://github.com/paritytech/polkadot/pull/1234"
-	//),
-	//Some((
-	//"https://github.com/paritytech/polkadot/pull/1234".to_owned(),
-	//"paritytech".to_owned(),
-	//"polkadot".to_owned(),
-	//1234
-	//))
-	//);
-	//assert_eq!(
-	//companion_parse(
-	//"\nthis is a companion pr https://github.com/paritytech/polkadot/pull/1234"
-	//),
-	//Some((
-	//"https://github.com/paritytech/polkadot/pull/1234".to_owned(),
-	//"paritytech".to_owned(),
-	//"polkadot".to_owned(),
-	//1234
-	//))
-	//);
-	//assert_eq!(
-	//companion_parse(
-	//"\nthis is some other pr https://github.com/paritytech/polkadot/pull/1234"
-	//),
-	//None,
-	//);
-	//assert_eq!(
-	//companion_parse(
-	//"\nthis is a companion pr https://github.com/paritytech/polkadot/pull/1234/plus+some&other_stuff"
-	//),
-	//Some((
-	//"https://github.com/paritytech/polkadot/pull/1234".to_owned(),
-	//"paritytech".to_owned(),
-	//"polkadot".to_owned(),
-	//1234
-	//))
-	//);
-	//assert_eq!(
-	//companion_parse("companion\nparitytech/polkadot#1234"),
-	//None
-	//);
-	//assert_eq!(
-	//companion_parse("companion: paritytech/polkadot#1234"),
-	//Some((
-	//"https://github.com/paritytech/polkadot/pull/1234".to_owned(),
-	//"paritytech".to_owned(),
-	//"polkadot".to_owned(),
-	//1234
-	//))
-	//);
-	//assert_eq!(
-	//companion_parse("companion: paritytech/polkadot/1234"),
-	//None
-	//);
-	//assert_eq!(
-	//companion_parse("stuff\ncompanion pr: paritytech/polkadot#1234"),
-	//Some((
-	//"https://github.com/paritytech/polkadot/pull/1234".to_owned(),
-	//"paritytech".to_owned(),
-	//"polkadot".to_owned(),
-	//1234
-	//))
-	//);
-	//}
+	#[test]
+	fn test_companion_parse() {
+		// Extra params should not be included in the parsed URL
+		assert_eq!(
+			companion_parse(
+				"companion: https://github.com/paritytech/polkadot/pull/1234?extra_params=true"
+			),
+			Some((
+				"https://github.com/paritytech/polkadot/pull/1234".to_owned(),
+				"paritytech".to_owned(),
+				"polkadot".to_owned(),
+				1234
+			))
+		);
+
+		// Long version should work even if the body has some other content around the companion
+		// text
+		assert_eq!(
+			companion_parse(
+				"Companion line is in the middle
+                companion: https://github.com/paritytech/polkadot/pull/1234
+                Final line"
+			),
+			Some((
+				"https://github.com/paritytech/polkadot/pull/1234".to_owned(),
+				"paritytech".to_owned(),
+				"polkadot".to_owned(),
+				1234
+			))
+		);
+
+		// Short version should work even if the body has some other content around the companion
+		// text
+		assert_eq!(
+			companion_parse(
+				"Companion line is in the middle
+                companion: paritytech/polkadot#1234
+                Final line"
+			),
+			Some((
+				"https://github.com/paritytech/polkadot/pull/1234".to_owned(),
+				"paritytech".to_owned(),
+				"polkadot".to_owned(),
+				1234
+			))
+		);
+
+		// Long version should not be detected if "companion: " and the expression are not both in
+		// the same line
+		assert_eq!(
+			companion_parse(
+				"I want to talk about companion: but NOT reference it
+                I submitted it in https://github.com/paritytech/polkadot/pull/1234"
+			),
+			None
+		);
+
+		// Short version should not be detected if "companion: " and the expression are not both in
+		// the same line
+		assert_eq!(
+			companion_parse(
+				"I want to talk about companion: but NOT reference it
+                I submitted it in paritytech/polkadot#1234"
+			),
+			None
+		);
+	}
 }
