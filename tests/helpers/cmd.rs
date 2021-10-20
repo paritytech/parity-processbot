d2fdbafe80c377e84b2c0f7a81a48a1c7ad62d32
joao-paulo-parity <joao-paulo@parity.io>
2021-10-20 13:39:14 -0300

    adapt command interface for success/failure

--- tests/helpers/cmd.rs
+++ tests/helpers/cmd.rs
@@ -9,6 +9,25 @@ pub enum CmdConfiguration<'a> {
 	IgnoreStderrStartingWith(&'a [&'a str]),
 }
 
+fn build_cmd<Cmd, Dir>(cmd: Cmd, args: &[&str], dir: &Option<Dir>) -> Command
+where
+	Cmd: AsRef<OsStr> + Display,
+	Dir: AsRef<Path> + Debug,
+{
+	let mut cmd = Command::new(cmd);
+
+	cmd.args(args);
+
+	if let Some(dir) = dir {
+		cmd.current_dir(dir);
+		println!("Executing {:?} on {:?}", cmd, dir);
+	} else {
+		println!("Executing {:?}", cmd);
+	}
+
+	cmd
+}
+
 pub fn exec<Cmd, Dir>(
 	cmd: Cmd,
 	args: &[&str],
@@ -18,17 +37,7 @@ pub fn exec<Cmd, Dir>(
 	Cmd: AsRef<OsStr> + Display,
 	Dir: AsRef<Path> + Debug,
 {
-	let mut cmd = Command::new(cmd);
-	let cmd = {
-		let cmd = cmd.args(args).stdout(Stdio::null());
-		if let Some(dir) = dir {
-			println!("Executing {:?} on {:?}", cmd, dir);
-			cmd.current_dir(dir)
-		} else {
-			println!("Executing {:?}", cmd);
-			cmd
-		}
-	};
+	let mut cmd = build_cmd(cmd, args, &dir);
 
 	let was_success = match conf {
 		Some(CmdConfiguration::IgnoreStderrStartingWith(
@@ -65,6 +74,19 @@ pub fn exec<Cmd, Dir>(
 	}
 }
 
+pub fn get_cmd_success<Cmd, Dir>(
+	cmd: Cmd,
+	args: &[&str],
+	dir: Option<Dir>,
+) -> bool
+where
+	Cmd: AsRef<OsStr> + Display,
+	Dir: AsRef<Path> + Debug,
+{
+	let mut cmd = build_cmd(cmd, args, &dir);
+	cmd.spawn().unwrap().wait().unwrap().success()
+}
+
 pub fn get_cmd_output<Cmd, Dir>(
 	cmd: Cmd,
 	args: &[&str],
@@ -72,20 +94,14 @@ pub fn get_cmd_output<Cmd, Dir>(
 ) -> String
 where
 	Cmd: AsRef<OsStr> + Display,
-	Dir: AsRef<Path>,
+	Dir: AsRef<Path> + Debug,
 {
-	let mut cmd = Command::new(cmd);
-	let cmd = {
-		let cmd = cmd.args(args).stdout(Stdio::piped());
-		if let Some(dir) = dir {
-			cmd.current_dir(dir)
-		} else {
-			cmd
-		}
-	};
-
-	println!("Getting output of {:?}", cmd);
-
-	let output = cmd.spawn().unwrap().wait_with_output().unwrap();
+	let mut cmd = build_cmd(cmd, args, &dir);
+	let output = cmd
+		.stdout(Stdio::piped())
+		.spawn()
+		.unwrap()
+		.wait_with_output()
+		.unwrap();
 	String::from_utf8_lossy(&output.stdout).trim().to_string()
 }
