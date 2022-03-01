9977e65db25c3dc335a3dac944f940fe4448d1a3
joao-paulo-parity <joao-paulo@parity.io>
2022-03-01 13:07:14 -0300

    update teams api
    https://github.blog/changelog/2022-02-22-sunset-notice-deprecated-teams-api-endpoints/

--- src/github_bot/team.rs
+++ src/github_bot/team.rs
@@ -1,34 +1,18 @@
 use crate::{github, Result};
-use futures_util::TryFutureExt;
 
 use super::GithubBot;
 
 impl GithubBot {
-	pub async fn team(&self, owner: &str, slug: &str) -> Result<github::Team> {
-		let url =
-			format!("{}/orgs/{}/teams/{}", self.github_api_url, owner, slug);
-		self.client.get(url).await
-	}
-
 	pub async fn team_members(
 		&self,
-		team_id: i64,
+		org: &str,
+		team: &str,
 	) -> Result<Vec<github::User>> {
 		self.client
 			.get_all(format!(
-				"{}/teams/{}/members",
-				self.github_api_url, team_id,
+				"{}/orgs/{}/teams/{}/members",
+				self.github_api_url, org, team,
 			))
-			.await
-	}
-
-	pub async fn team_members_by_team_name(
-		&self,
-		owner: &str,
-		team: &str,
-	) -> Result<Vec<github::User>> {
-		self.team(owner, team)
-			.and_then(|team| self.team_members(team.id))
 			.await
 	}
 }
