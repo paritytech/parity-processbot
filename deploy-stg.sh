228942a2b8b62d3c6310b6807e3de1a4b6de711c
Maksym Hlukhovtsov <1177472+mordamax@users.noreply.github.com>
2023-02-16 13:26:03 +0100

    Mak/fix stg (#409)
    * move to INSTALLATION_LOGIN env var from gitlab
    
    * Create deploy-stg.sh
    
    ---------
    
    Co-authored-by: alvicsam <alvicsam@gmail.com>

---
+++ deploy-stg.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+timestamp=$(date +%s)
+
+# remember initial branch
+dev_branch=$(git rev-parse --abbrev-ref HEAD)
+
+# replace possible "/" with "-"
+# Because of a docker, as it uses git tag for tagging the image so if there's a / symbol
+# docker thinks that it's actually a path. but not part of the tag name.
+dev_branch_sanitized=${dev_branch/\//-}
+
+stg_branch="stg-v0.0.${timestamp}-${dev_branch_sanitized}"
+
+# create a branch on remote
+git push origin HEAD:"$stg_branch"
+
+# wait a bit before deleting branch, so gitlab triggers pipeline
+sleep 10
+
+git push origin --delete "$stg_branch"
